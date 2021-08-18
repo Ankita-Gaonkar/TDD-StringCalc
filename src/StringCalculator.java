3b4e5bc84b7f14ddfa89949ca81aac4f7815df1c
Ankita-Gaonkar <ankitagaonkar00@gmail.com>
2021-08-19 01:40:55 +0530

    Test case 1 passed

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,4 +1,11 @@
 
 public class StringCalculator {
 
+	public int add(String string) {
+		if(string.isEmpty()){
+            return 0;
+        }
+        return Integer.parseInt(string); 
+ 	}
+
 }
--- src/TestCases.java
+++ src/TestCases.java
@@ -1,4 +1,23 @@
+import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+@Test
 public class TestCases {
+	private StringCalculator calculator =new StringCalculator();
 
-}
+    @BeforeTest
+    public void init(){
+        calculator =new StringCalculator();
+    }
+     
+    public void emptyStringReturnsZero() throws Exception{
+        assertEquals(calculator.add(""),0);
+    }
+    
+    public void singleValueIsReplied() throws Exception{
+        assertEquals(calculator.add("1"),1);
+    }
+}   
+
